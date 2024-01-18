0649df82d4b6b34a263912f0aca361988b700699
remziyesahin <100557948+remziyesahin@users.noreply.github.com>
2024-01-18 16:12:26 +0300

    Update streamlit_app.py
    Filtered the table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list['Fruit']))
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-
-streamlit.dataframe(my_fruit_list)
+fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruit_to_show = my_fruit_list.loc[fruit_selected]
+streamlit.dataframe(fruit_to_show)
 
