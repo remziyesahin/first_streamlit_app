a1029f792bedfac820761b738389d37ae0135629
remziyesahin <100557948+remziyesahin@users.noreply.github.com>
2024-01-22 10:31:20 +0300

    Added a text entry bocx and send the Input to Fruitvice as part of the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,9 @@ streamlit.dataframe(fruit_to_show)
 #New section to display fruityvice api response
 import requests
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
