a899f3b4077a54971bd97a84dbbf3b17486475a6
remziyesahin <100557948+remziyesahin@users.noreply.github.com>
2024-01-22 10:18:53 +0300

    Displayed Fruit Advice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,10 @@ fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruit_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruit_to_show)
 
+
+#New section to display fruityvice api response
+import requests
+streamlit.header('Fruityvice Fruit Advice!')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+
